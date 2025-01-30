ff6afda29a22675e6d355b9582d5c26b88718e64
And-r-b <and.r.b@hotmail.com>
2025-01-30 11:32:17 +0100

    added datatransfer

---
+++ DataTransferObjects.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.SignalR;
+
+class BorrowedRequest
+{
+    public required string Message { get; set; }
+    public required int NUmber { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
 using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Http.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => {
-    var jsonPayload = new {message = "Hello"};
-    return Results.Ok("All went well");
+    var jsonPayload = new {message = "Hello", content = "Testing testing"};
+    return Results.Ok(jsonPayload);
 });
 
-app.MapPost("/", () => {
-    return Results.BadRequest();
+app.MapPost("/", (BorrowedRequest requestBody) => {
+    Console.WriteLine{$"Message: {requestBody.Message}"};
+    Console.WriteLine{$"Number: {requestBody.Number}"};
+    return Results.Accepted();
 });
 
 app.MapPut("/", () => {
+    Console.WriteLine($"At dynamic segment");
     return Results.Unauthorized();
 });
 
